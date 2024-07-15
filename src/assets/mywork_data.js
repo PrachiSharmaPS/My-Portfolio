da74e1b1ee55eead733274d24c16e64dcebd697f
PrachiSharmaPS <prachisharma.ps01@gmail.com>
2024-07-15 07:29:18 +0530

    UI and basic functionality done

---
+++ src/assets/mywork_data.js
@@ -0,0 +1,30 @@
+import project1_img from '../assets/project_1.png'
+import project2_img from '../assets/project_2.png'
+import project3_img from '../assets/project3_img.png'
+import project4_img from '../assets/project_4.svg'
+import project5_img from '../assets/project_5.svg'
+import project6_img from '../assets/project_6.svg'
+
+const mywork_data = [
+    {
+        w_no:1,
+        w_name:"Web App",
+        w_img:project1_img,
+        w_description: "Build Backend Logic for the Luxury watch lisitng web site. Use Mail Modo as Mail serviceFeatured Watch is a platform designed to streamline the buying and selling process of luxury watches. The project utilizes MailModo as its mail service provider, ensuring efficient and reliable email communication for users. With its user-friendly interface and intuitive functionality, users can achieve their watch-related transactions with ease, all within a few clicks."
+    },
+    {
+        w_no:2,
+        w_name:"Web App",
+        w_img:project2_img,
+        w_description:""
+    },
+    {
+        w_no:3,
+        w_name:"Web App",
+        w_img:project3_img,
+        w_description:""
+    },
+    
+]
+ 
+export default mywork_data;
